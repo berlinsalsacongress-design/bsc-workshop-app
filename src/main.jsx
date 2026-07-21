0b054ad64ce38df8482422614fba6d7ad6b09691
berlinsalsacongress-design <berlinsalsacongress@gmail.com>
2026-07-21 12:59:16 +0000

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './src/index.css';
 import App from "./App";
-
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
